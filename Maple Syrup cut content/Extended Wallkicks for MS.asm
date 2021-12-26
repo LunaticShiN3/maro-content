26fd30b6f8aeceb3d74dfccc902d4bd3fb9bbeb9
LunaticShiN3 <96666614+LunaticShiN3@users.noreply.github.com>
2021-12-26 23:03:15 +0100

    -UNTESTED- Dive wallkicks for MS?
    An edited version of my extended wallkicks / dive wallkicks patch designed for MS.
    Unsure of what the changes are or how it's meant to work.
    Please use with caution or refer to the LU1.5 folder.

--- Maple Syrup cut content/Extended Wallkicks for MS.asm
+++ Maple Syrup cut content/Extended Wallkicks for MS.asm
@@ -38,21 +38,21 @@ SB T9, 0x7500(T8)
 LUI T1, 0x8034
 ADDIU T1, T1, 0xB170
 
-LW T2, 0x10(T1) ;;;load previous action variable;;;
+LW T2, 0x10(T1) ;load previous action variable
 ADDIU T3, R0, 0x08A7
-BEQ T2, T3, LetsGo ;;;branch if previous action is air hitting wall;;;
+BEQ T2, T3, LetsGo ;branch if previous action is air hitting wall
 NOP
 
 LUI T3, 0x0800
 ADDIU T3, T3, 0x034B
-BEQ T2, T3, LetsGo ;;;branch if previous action is ledgegrab;;;
+BEQ T2, T3, LetsGo ;branch if previous action is ledgegrab
 NOP
 
-LW T2, 0xC(T1) ;;;load action variable;;;
+LW T2, 0xC(T1) ;load action variable
 
 LUI T3, 0x0102
 ADDIU T3, T3, 0x08B6
-BNE T2, T3, HeFast ;;;branch if action isn't soft bonk;;;
+BNE T2, T3, HeFast ;branch if action isn't soft bonk
 NOP
 B DoIt
 NOP
@@ -60,14 +60,14 @@ NOP
 HeFast:
 LUI T3, 0x0102
 ADDIU T3, T3, 0x08B0
-BNE T2, T3, LetsGo ;;;branch if action isn't hard bonk;;;
+BNE T2, T3, LetsGo ;branch if action isn't hard bonk
 NOP
 
 DoIt:
 ADDIU T4, R0, 0x08A7
-SW T4, 0x10(T1) ;;;set previous action to air hitting wall;;;
+SW T4, 0x10(T1) ;set previous action to air hitting wall
 ADDIU T4, R0, 0xA
-SB T4, 0x2A(T1) ;;;set wallkick timer to 10;;;
+SB T4, 0x2A(T1) ;set wallkick timer to 10
 
 LetsGo:
 LUI T0, 0x802A
