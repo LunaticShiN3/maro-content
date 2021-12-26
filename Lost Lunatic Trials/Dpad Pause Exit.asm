2e111b7ea17387cc4f3072cbf2f4e9919987bd7e
LunaticShiN3 <96666614+LunaticShiN3@users.noreply.github.com>
2021-12-26 22:49:15 +0100

    Makes pause menu controls function with DPad.
    Instead of analog stick.
    Also affects star select, save menu, etc.
    Mainly used to prevent accidentally exiting course.
    Best used when combined with exit course while moving tweak.

--- Lost Lunatic Trials/Dpad Pause Exit.asm
+++ Lost Lunatic Trials/Dpad Pause Exit.asm
@@ -1,7 +1,7 @@
 .orga 0x93658
 LUI T7, 0x8034
 LB T7, 0xAFF8(T7)
-ANDI T7, T7, 0x0008
+ANDI T7, T7, 0x0008 ;each of these is for a different dpad direction, I figured them out by trial and error, cba to comment which is which
 NOP
 BEQZ T7, 0x9367C
 NOP
@@ -9,7 +9,7 @@ NOP
 .orga 0x9367C
 LUI T1, 0x8034
 LB T1, 0xAFF8(T1)
-ANDI T1, T1, 0x0004
+ANDI T1, T1, 0x0004 ;each of these is for a different dpad direction, I figured them out by trial and error, cba to comment which is which
 NOP
 BEQZ T1, 0x936A0
 NOP
@@ -17,7 +17,7 @@ NOP
 .orga 0x936B8
 LUI T6, 0x8034
 LB T6, 0xAFF8(T6)
-ANDI T6, T6, 0x0001
+ANDI T6, T6, 0x0001 ;each of these is for a different dpad direction, I figured them out by trial and error, cba to comment which is which
 NOP
 BEQZ T6, 0x936DC
 NOP
@@ -25,7 +25,7 @@ NOP
 .orga 0x936DC
 LUI T0, 0x8034
 LB T0, 0xAFF8(T0)
-ANDI T0, T0, 0x0002
+ANDI T0, T0, 0x0002 ;each of these is for a different dpad direction, I figured them out by trial and error, cba to comment which is which
 NOP
 BEQZ T0, 0x93700
 NOP
